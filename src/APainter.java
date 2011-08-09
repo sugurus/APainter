9add4faf95a990b1298cc2f0140ef996dc3a8c47
nodamushi <oreno_e_mail@yahoo.co.jp>
2011-08-09 09:55:50 +0900

    README追加

--- src/APainter.java
+++ src/APainter.java
@@ -1,120 +1,56 @@
-//import javax.swing.JApplet;
-import java.io.File;
-import java.net.MalformedURLException;
-import java.net.URL;
-
-import javax.swing.JComponent;
 import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
-import javax.swing.plaf.ComponentUI;
 
 import apainter.Device;
 import apainter.ExitListener;
-import apainter.annotation.Version;
-import apainter.canvas.Canvas;
 import apainter.canvas.CanvasHandler;
 
+/**
+ * ひとまずのGUI
+ * @author nodamushi
+ */
+public class APainter{
 
-@Version("0.1.0")
-public class APainter implements ExitListener
-//extends JApplet
-{
-	static private APainter instance=null;
-
-
-	static public JComponent getContentPane(){
-		if(instance.isApplet){
-			//TODO applet
-			return null;
-		}else return (JComponent) instance.frame.getContentPane();
-	}
-	public static synchronized void main(final String[] args) {
-		if(instance!=null)return;
-		instance = new APainter();
-		instance.isApplet = false;
-		instance.frame = new JFrame("APainter");
-		instance.init(args);
+	public static void main(final String[] argv) {
+		final APainter apainter = new APainter(argv);
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
-				instance.frame.setVisible(true);
+				apainter.frame.setVisible(true);
 			}
 		});
 	}
 
-	public static URL getURL(String path){
-		if(instance.isApplet){
-			//TODO applet
-			return null;
-		}else{
-			File f = new File(path).getAbsoluteFile();
-			try {
-				return f.toURI().toURL();
-			} catch (MalformedURLException e) {
-				return null;
-			}
-		}
-	}
 
-	private boolean isApplet;
 	private JFrame frame;
 
 	private apainter.APainter core;
 
-
-	public void init(String[] args){
-		//TODO init
+	public APainter(String[] argv) {
+		frame = new JFrame("APainter");
 		core = apainter.APainter.createAPainter(Device.CPU);
-		core.addExitListener(this);
+		core.addExitListener(new EL());
 		CanvasHandler canvas = core.createNewCanvas(401, 401);
 		frame.add(canvas.getComponent());
 		frame.pack();
 		frame.setDefaultCloseOperation(3);
 		core.debagON();
-
 	}
 
-	public void init() {
-		if(instance==null){
-			instance = this;
-		//TODO for JApplet
+	private static class EL implements ExitListener{
+		@Override
+		public boolean exiting(apainter.APainter apainter) {
+			return true;
 		}
-	}
 
-	public void start(){
-		//TODO for JApplet
-	}
+		@Override
+		public void exit(apainter.APainter apainter) {
 
+		}
 
-	public void stop(){
-		//TODO for JApplet
-	}
-
-	public void destroy(){
-		//TODO for JApplet
-	}
-
-	public void pack(){
-		if(!isApplet)frame.pack();
-	}
-
-	public void setTitle(String title){
-		if(!isApplet)frame.setTitle(title);
-	}
-
-
-	@Override
-	public boolean exiting(apainter.APainter apainter) {
-		return true;
-	}
-
-	@Override
-	public void exit(apainter.APainter apainter) {
-
-	}
-
-	@Override
-	public void exited(apainter.APainter apainter) {
-		System.exit(0);
+		@Override
+		public void exited(apainter.APainter apainter) {
+			System.exit(0);
+		}
 	}
 
 }
